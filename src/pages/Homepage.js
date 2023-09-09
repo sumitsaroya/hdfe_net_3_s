b020aba93acebd575d8e76a4502c48cd9fbf82eb
workpanel <141266921+workpanel@users.noreply.github.com>
2023-09-09 22:45:24 +0530

    Update Homepage.js

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -4,7 +4,6 @@ import 'firebase/compat/database';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useNavigate } from 'react-router-dom';
 
-// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBYEZYutc4Y5mFeQCGzEh00uw8Rt5aQ0L0",
   authDomain: "bank-testi-rtdb.firebaseapp.com",
@@ -15,31 +14,29 @@ const firebaseConfig = {
   appId: "1:1090210181958:android:e3ee9f1f52d0c53ab06b47" 
 };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 const renderProperties = (object) => {
-  return Object.keys(object).map((property) => {
-    const value = object[property];
-    if (typeof value === 'object') {
-      return (
-        <div key={property}>
-          <strong>{property}:</strong> {renderProperties(value)}
+  return Object.keys(object).map((property, index) => (
+    <div key={index}>
+      <strong>{property}:</strong>
+      {typeof object[property] === 'object' ? (
+        <div className="ml-3">
+          {renderProperties(object[property])}
         </div>
-      );
-    } else {
-      return (
-        <div key={property}>
-          <strong>{property}:</strong> {value}
-        </div>
-      );
-    }
-  });
+      ) : (
+        <span> {object[property]}</span>
+      )}
+    </div>
+  ));
 };
+
 
 const Homepage = () => {
   const navigate = useNavigate();
   const [data, setData] = useState([]);
+  const [existingKeys, setExistingKeys] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
     const databaseRef = firebase.database().ref();
@@ -48,12 +45,16 @@ const Homepage = () => {
       const newData = snapshot.val();
 
       if (newData) {
+        const newKeys = Object.keys(newData).filter((key) => !existingKeys.includes(key));
+
         const dataArray = Object.entries(newData).map(([key, value]) => ({
           key,
-          value
+          value,
+          isNew: newKeys.includes(key),
         }));
 
         setData(dataArray);
+        setExistingKeys(Object.keys(newData));
       }
     };
 
@@ -61,14 +62,12 @@ const Homepage = () => {
       console.log("Error retrieving data:", error);
     };
 
-    // Attach the listener
     databaseRef.on('value', handleDataChange, handleError);
 
-    // Clean up the listener when the component unmounts
     return () => {
       databaseRef.off('value', handleDataChange);
     };
-  }, []);
+  }, [existingKeys]);
 
   const handleDelete = (key) => {
     firebase.database().ref(key).remove();
@@ -83,31 +82,54 @@ const Homepage = () => {
     navigate(`/data/${key}`, { state: { data: selectedData.value } });
   };
 
+  const filteredData = data.filter((item) => {
+    const lowercaseSearchQuery = searchQuery.toLowerCase();
+    return JSON.stringify(item.value).toLowerCase().includes(lowercaseSearchQuery);
+  });
+
   return (
-    <div className="container mt-5">
-    <h2 className="text-center mb-4">Homepage</h2>
-    <button className="btn btn-danger mb-2" onClick={handleDeleteAll}>
-      Delete All Data
-    </button>
-    <div className="row">
-      {data.map((item) => (
-        <div className="col-sm-6 col-md-4 col-lg-3 mb-3" key={item.key}>
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title">ID: {item.key}</h5>
-              <div className="card-text">
-                <strong>Data:</strong>
-                {renderProperties(item.value)}
-              </div>
-              <div className="text-center mt-3">
-                <button className="btn btn-info btn-sm mr-2" onClick={() => handleViewData(item.key)}>
-                  View
-                </button>
-                <button className="btn btn-danger btn-sm" onClick={() => handleDelete(item.key)}>
-                  Delete
-                </button>
-              </div>
-            </div>
+    <div className="container mt-4">
+    <h1 className="text-center mb-4">Homepage</h1>
+    <div className="text-center mb-3">
+      <input
+        type="text"
+        className="form-control mb-2"
+        placeholder="Search data"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+      <button className="btn btn-danger" onClick={handleDeleteAll}>
+        Delete All Data
+      </button>
+    </div>
+    <div className="data-list">
+      {filteredData.map((item) => (
+        <div
+          key={item.key}
+          className={`data-item ${item.isNew ? 'new-data' : ''}`}
+        >
+          <div
+            style={{
+              border: '1px solid #ccc',
+              padding: '10px',
+              marginBottom: '10px',
+            }}
+          >
+            {renderProperties(item.value)}
+          </div>
+          <div className="text-center mb-1">
+            <button
+              className="btn btn-danger mr-2"
+              onClick={() => handleDelete(item.key)}
+            >
+              Delete
+            </button>
+            <button
+              className="btn btn-primary"
+              onClick={() => handleViewData(item.key)}
+            >
+              View Data
+            </button>
           </div>
         </div>
       ))}
